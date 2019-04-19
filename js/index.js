b73183947b1038d52fcbbb19cbfefcc45b364b9e
= <arthurcastroes@gmail.com>
2019-04-18 21:48:39 -0300

    Added functions pegaElementosForm e adicionaResultado

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
 let formChurras = document.getElementById('cc-churras-form');
-console.log(formChurras);
+let resultadoChurrasDiv = document.getElementById('resultadoChurras');
+//console.log(formChurras);
 formChurras.addEventListener('submit', (event)=> {
     event.preventDefault();
+    console.log("Preparando churrasco...")
+    pegaElementosForm();
+    adicionaResultado();
 });     
+
+function pegaElementosForm(){
+    let adultos = document.getElementById('pessoasInput').value;
+    let criancas = document.getElementById('criancasInput').value;
+
+    /* PEGA O NOME DOS CHECKBOXES */
+    let carneAlcatra = document.getElementById('tp-alcatara').name;
+    let carneSol = document.getElementById('tp-sol').name;
+    let carneContra = document.getElementById('tp-contra').name;
+    let carnePicanha = document.getElementById('tp-picanha').name;
+    let carneMaminha = document.getElementById('tp-maminha').name;
+    let carneCordeiro = document.getElementById('tp-cordeiro').name;
+
+    
+}
+
+function adicionaResultado(){
+
+}
+
