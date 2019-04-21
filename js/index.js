6b322177bd8f1093ce913852cf16b0162ff831cd
= <arthurcastroes@gmail.com>
2019-04-21 15:20:21 -0300

    criei uma funcao que pega todos os checkboxes e os que estao marcados

--- js/index.js
+++ js/index.js
@@ -3,28 +3,61 @@ let resultadoChurrasDiv = document.getElementById('resultadoChurras');
 //console.log(formChurras);
 formChurras.addEventListener('submit', (event)=> {
     event.preventDefault();
-    console.log("Preparando churrasco...")
-    pegaElementosForm();
-    adicionaResultado();
+    console.log("Preparando churrasco...");
+    fazPaginaComLista();
 });     
 
 function pegaElementosForm(){
     let adultos = document.getElementById('pessoasInput').value;
     let criancas = document.getElementById('criancasInput').value;
-
-    /* PEGA O NOME DOS CHECKBOXES */
-    let carneAlcatra = document.getElementById('tp-alcatara').name;
-    let carneSol = document.getElementById('tp-sol').name;
-    let carneContra = document.getElementById('tp-contra').name;
-    let carnePicanha = document.getElementById('tp-picanha').name;
-    let carneMaminha = document.getElementById('tp-maminha').name;
-    let carneCordeiro = document.getElementById('tp-cordeiro').name;
-
-    //falta implementar logica, para checar se o checkbox está marcado, e se estive entao pegar o nome do checkbox marcado e 
-    //adicionar essa string ao array dentro de um objeto chamado Churrasco
 }
 
-function adicionaResultado(){
+function pegaCheckboxesDaPagina(){
+    //carnes
+    let carneAlcatra = document.getElementById('tp-alcatara');
+    let carneSol = document.getElementById('tp-sol');
+    let carneContra = document.getElementById('tp-contra');
+    let carnePicanha = document.getElementById('tp-picanha');
+    let carneMaminha = document.getElementById('tp-maminha');
+    let carneCordeiro = document.getElementById('tp-cordeiro');
+    //linguicas
+    let lingFrango = document.getElementById('ling-frango');
+    let lingToscana = document.getElementById('ling-toscana');
+    //carnes de frango
+    let carneFrangoCoracao = document.getElementById('cf-coracao');
+    let carneFrangoSobrecoxa = document.getElementById('cf-sobrecoxa');
+    //bebidas e outros
+    let bebidaCerveja600 = document.getElementById('b-cerveja600');
+    let bebidaCervejaLataLong = document.getElementById('b-cervja-lata-long');
+    let bebidaRefrigerante = document.getElementById('b-refrigerante');
+    let comidaPaes = document.getElementById('c-paes');
+    let comidaFarofa = document.getElementById('c-farofa');
+    let outrosCarvao = document.getElementById('o-carvao');
 
+    return [
+            carneAlcatra, carneSol, carneContra, carnePicanha, carneMaminha, carneCordeiro, 
+            lingFrango, lingToscana,
+            carneFrangoCoracao, carneFrangoSobrecoxa,
+            bebidaCerveja600, bebidaCervejaLataLong, bebidaRefrigerante,
+            comidaPaes, comidaFarofa,
+            outrosCarvao
+    ];
+}
+
+function pegaCheckboxMarcados(items){
+    let itemsChecados = [];
+    items.forEach(item => {
+        if(item.checked == true){
+            console.log("Item checado: " + item.name);
+            itemsChecados.push(item.name);
+        }
+    });
+    return itemsChecados;
+}
+
+function fazPaginaComLista(){
+    let opened = window.open("teste");
+    opened.document.write("<html><head><title>Hello pagina novaa linda</title></head><body>testeoaooasodkas</body></html>");
+    //opened.print();
 }
 
