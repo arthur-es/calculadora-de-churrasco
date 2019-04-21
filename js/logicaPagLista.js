c5dcc925ffc6801dc23ca9839d540ef8e60a0086
= <arthurcastroes@gmail.com>
2019-04-21 18:42:08 -0300

    ultimas mudancas por hoje

--- js/logicaPagLista.js
+++ js/logicaPagLista.js
@@ -1,3 +1,25 @@
+let buttonImprimirLista = document.getElementById('imprimirLista');
+buttonImprimirLista.addEventListener('click', (event)=> {
+    event.preventDefault();
+    console.log("Imprimindo lista....");
+    escondeBotao();
+    this.print();
+}); 
+
+
+function escondeBotao(){
+    buttonImprimirLista.classList.add('botaoEscondido');
+
+    setInterval(function(){ 
+        mostraBotao();
+    }, 1000);
+}
+
+function mostraBotao() {
+    buttonImprimirLista.classList.remove('botaoEscondido');
+}
+
+
 let ItensLista = JSON.parse(localStorage.getItem('ItemsLista'));
 
 let divChurrascao = document.getElementById("listaChurrascao");
